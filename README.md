53bba0b21f1586489fbf550ffe91884329132aaa
mragapp <ksvrag40@gmail.com>
2025-06-22 22:55:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample Python Project
+
+This is a sample Python project to demonstrate documentation with Sphinx and Read the Docs. 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sampleproject',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='A sample Python project for documentation demo',
+) 
