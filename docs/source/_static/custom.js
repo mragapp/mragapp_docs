cf44a39ef177ea64d1d10bc52a9979120c0cf6bd
ravihfac <ravikumarhfac@gmail.com>
2025-06-28 00:06:34 +0530

    commit

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -1,6 +1,22 @@
+// docs/_static/custom.js
+
+function removeFlyout() {
+  const flyout = document.querySelector("readthedocs-flyout");
+  if (flyout) {
+    flyout.remove();
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    const flyout = document.querySelector("readthedocs-flyout");
-    if (flyout) {
-      flyout.remove();
-    }
+  removeFlyout();
+
+  // Observe DOM changes and remove the flyout when it appears again
+  const observer = new MutationObserver(() => {
+    removeFlyout();
   });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+});
